97c90126dab5d02d841212e291c7cac30262685f
akshitagarwal123 <akshitagarwal10505@gmail.com>
2020-09-22 22:46:47 +0530

    added buttons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
 export class Counter extends Component {
     state={
-        count:1,
+        count:this.props.value,
         tags: ['tag1','tag2','tag3']
     };
     styles={
         fontSize:10,
         fontWeight:'bold'
     }
+    
+    handleIncrement=(param)=>{
+        console.log(param);
+        this.setState({count:this.state.count+1});
+    };
+    doHandleIncrement=()=>{
+        this.handleIncrement({id:1});
+    }
     render() {
-        let classes = this.getBadgeClasses();
+        console.log("props",this.props);
         return(
              <div>
-          
-        <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+         <button  onClick={this.doHandleIncrement} className="btn btn-secondary btn-sm">Increment</button> 
+            <button className="btn btn-danger btn-sm m-2">
+                Delete
+            </button>
             </div>
         );
     }
    
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const { count } =this.state;
+        return count===0?'Zero' : count;
+    }
 }
  
 export default Counter;
